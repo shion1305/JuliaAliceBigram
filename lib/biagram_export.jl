b20a1513b654dadc2c6ce7bb992d0291d9aa05bc
Shion Ichikawa <shion1305@gmail.com>
2023-11-09 20:44:16 +0000

    ✨ create graph for each cases, update dataset

--- lib/biagram_export.jl
+++ lib/biagram_export.jl
@@ -1,8 +1,9 @@
 using Plots
+
 using Measures
 function generate_bigram_graph(bigram_data, filename)
-    entropies = [count / total_counts for (_, _, count) in bigram_data]
-    labels = [string(first, " ", second) for (first, second, _) in bigram_data]
+    entropies = [value for (_, value) in bigram_data[1]]
+    labels = [string(words[1], " ", words[2]) for (words, _) in bigram_data[1]]
     bar_plot = bar(labels, entropies, title="Bigram Entropies", xlabel="Bigrams", ylabel="Entropy",
         legend=false, xrotation=45, xticks=(1:length(labels), labels), size=(800, 600), margin=5mm, xguidefontsize=10, yguidefontsize=10)
     savefig(bar_plot, filename * ".png")
